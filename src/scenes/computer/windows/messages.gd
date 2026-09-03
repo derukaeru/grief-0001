c6bbb83348c6b481e7a4b06c4f83f92779daa93f
dalifyyy <d4lified@gmail.com>
2026-09-03 21:06:37 +0800

    feat: added partial messages window functionality

---
+++ src/scenes/computer/windows/messages.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@onready var messages_container: VBoxContainer = $messages
+@onready var message: Control = $message
+
+var messages: Array = [
+	"Lebon James",
+]
+
+func _ready() -> void:
+	for entry in messages:
+		if not Dialogues.MESSAGES.has(entry): return
+		var dialogue: Array = Dialogues.MESSAGES[entry]
+		
+		var button: Button = Button.new()
+		messages_container.add_child(button)
+		
+		var text: String = entry + ": " + dialogue[-1][-1]
+		text = text.substr(0, 26)
+		
+		button.text = text + "..."
+		button.set_text_alignment(HORIZONTAL_ALIGNMENT_LEFT)
+		button.pressed.connect(func() -> void: open_message(entry))
+		button.text_direction = Control.TEXT_DIRECTION_LTR
+
+func open_message(id: String) -> void:
+	messages_container.hide()
+	var dialogue: Array = Dialogues.MESSAGES[id]
+	
+	# todo here
+	
+	message.show()
+
+func back() -> void:
+	messages_container.show()
+	message.hide()
--- src/scripts/autoloads/dialogues.gd
+++ src/scripts/autoloads/dialogues.gd
@@ -5,3 +5,12 @@ var dialogue: Dictionary = {
 		"dialogue_1": ["line 1", "line 2"]
 	}
 }
+
+# structured as messages are alternating, the arrays in the id are for double or multiple texts for the same person
+var MESSAGES: Dictionary[String, Array] = {
+	"Lebon James": [
+		["hello", "this is the real lebron james"],
+		["this is a scam", "who are you"],
+		["this is the real lebron james"],
+	]
+}
